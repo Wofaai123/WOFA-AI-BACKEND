30cb6f7132de55d4b48c1549c5e05877b8844ecf
Wofaai123 <revjj2018@gmail.com>
2026-02-03 09:57:41 +0000

    Add files via upload

---
+++ models/Progress.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const ProgressSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+  lesson: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Lesson",
+    required: true
+  },
+  completed: {
+    type: Boolean,
+    default: false
+  },
+  completedAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model("Progress", ProgressSchema);
